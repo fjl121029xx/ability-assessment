2a41f4c37bae816c493303f61a64404f971d9a70
e <sishengqikuo_xx@163.com>
2018-10-19 09:24:42 +0800

    2018-10-19

--- src/main/java/com/li/ability/assessment/AbilityAssessment.scala
+++ src/main/java/com/li/ability/assessment/AbilityAssessment.scala
@@ -11,12 +11,13 @@ object AbilityAssessment {
 
   def main(args: Array[String]): Unit = {
 
+
     val inputUrl = "mongodb://huatu_ztk:wEXqgk2Q6LW8UzSjvZrs@192.168.100.153:27017,192.168.100.154:27017,192.168.100.155:27017/huatu_ztk"
     val collection = "ztk_answer_card"
 
     val conf = new SparkConf()
       .setAppName("AbilityAssessment")
-      .setMaster("local[13]")
+      //      .setMaster("local[13]")
       .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
 
     val sql = SparkSession.builder
@@ -37,25 +38,35 @@ object AbilityAssessment {
       )
     ).load
 
+    /**
+      * 全站数据
+      */
+    val total_station = mongo.select("userId", "subject", "catgory", "expendTime", "createTime", "corrects", "paper.questions", "paper.modules", "times")
+    //      .map(t => (t.getLong(0), t.getInt(1), t.getInt(2), t.getInt(3), t.getLong(4), t.getSeq(5), t.getSeq(6), t.getSeq(7), t.getSeq(8)))
+    //      .rdd
+    total_station.cache()
 
-    val answer_card_df = mongo.select( "userId", "subject", "catgory", "expendTime", "createTime", "corrects", "paper.questions", "paper.modules","times")
-      .rdd
-      .map(t => (t.getLong(0), t.getInt(1), t.getInt(2), t.getInt(3), t.getLong(4), t.getSeq(5), t.getSeq(6), t.getSeq(7), t.getSeq(8)))
+    //    val resut = total_station.map(t => (t.getLong(0), (t.getInt(1), t.getInt(2), t.getInt(3), t.getLong(4), t.getSeq(5), t.getSeq(6), t.getSeq(7), t.getSeq(8))))
+    //      .rdd
 
-    answer_card_df.cache()
+    val resut = total_station.map(t => (t.getLong(0), 1)).rdd.reduceByKey(_ + _)
+
+    //    val last_week = total_station.filter(f =>
+    //      TimeUtils.convertTimeStamp2DateStr(System.currentTimeMillis() - 7 * 86400000L, "yyyy-w").compareTo(TimeUtils.convertTimeStamp2DateStr(f._4, "yyyy-w")) == 0
+    //    )
 
 
-//    answer_card_df.flatMap(f=>{
-//      val arr = new ArrayBuffer[Tuple10[]]()
+    //    answer_card_df.flatMap(f=>{
+    //      val arr = new ArrayBuffer[Tuple10[]]()
 
-//      arr.iterator
-//    })
-    answer_card_df.take(10000).foreach(println)
+    //      arr.iterator
+    //    })
+    //    answer_card_df.take(1).foreach(println)
 
     //    answer_card_df.take(1).foreach(println)
     //   val answer_card_rdd = answer_card_df.rdd.repartition(1)
     //    answer_card_df.rdd.saveAsTextFile("hdfs://master/ztk_question_record/ztk_answer_card/")
-    //    answer_card_df.write.json("hdfs://master/ztk_question_record/ztk_answer_card/")
+    resut.saveAsTextFile(args(0))
   }
 
 }
